1705901bf8f44f7ef9a78173196d35984f4d3484
Anand Baby <anandbabyv@gmail.com>
2026-05-01 18:23:19 +0530

    Adding new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,22 +16,20 @@ jQuery(document).ready(function($) {
 
     });
    
-    document.addEventListener("DOMContentLoaded", function() {
-      // Experiences section links
-      document.querySelectorAll('.experiences-section a').forEach(function(link) {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
-      });
-      // Projects section links
-      document.querySelectorAll('.projects-section a').forEach(function(link) {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
-      });
-      // Publications/blogs section links
-      document.querySelectorAll('.publications-section a').forEach(function(link) {
-        link.setAttribute('target', '_blank');
-        link.setAttribute('rel', 'noopener noreferrer');
-      });
+    // Experiences section links
+    document.querySelectorAll('.experiences-section a').forEach(function(link) {
+      link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener noreferrer');
+    });
+    // Projects section links
+    document.querySelectorAll('.projects-section a').forEach(function(link) {
+      link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener noreferrer');
+    });
+    // Publications/blogs section links
+    document.querySelectorAll('.publications-section a').forEach(function(link) {
+      link.setAttribute('target', '_blank');
+      link.setAttribute('rel', 'noopener noreferrer');
     });
 
 });
